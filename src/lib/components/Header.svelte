12da1899fee7a2be7c9f44cf252530fe19195869
Philip Washburn <vpwash@gmail.com>
2025-05-18 21:25:27 -0500

    Added DaisyUI in rules

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -40,6 +40,7 @@
         <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
         </div>
+        <!-- svelte-ignore a11y_no_noninteractive_tabindex -->
         <ul
           tabindex="0"
           class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
